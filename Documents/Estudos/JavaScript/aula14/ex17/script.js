eca5317319b625b98af7caebca78859d9db78f46
danielanakazawa <ds.nakazawa@gmail.com>
2021-07-15 14:35:04 -0300

    Inserindo comando limpar

--- Documents/Estudos/JavaScript/aula14/ex17/script.js
+++ Documents/Estudos/JavaScript/aula14/ex17/script.js
@@ -9,6 +9,7 @@ if (num.value.length == 0){
 } else {
     var n = Number (num.value)
     
+    tab.innerHTML= ''
     for (var c = 0; c <= 10 ; c++){
         var item = document.createElement('option')
         var cal = Number (n*c)
